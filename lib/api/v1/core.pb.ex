8fc0e7005fa5b0b497edff1d340ce5e9bc1b2a5b
Bas <bas.vanooyen@bettyblocks.com>
2023-02-13 16:48:07 +0100

    run formatter

--- lib/api/v1/core.pb.ex
+++ lib/api/v1/core.pb.ex
@@ -2,109 +2,125 @@ defmodule Authzed.Api.V1.RelationshipUpdate.Operation do
   @moduledoc false
   use Protobuf, enum: true, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :OPERATION_UNSPECIFIED, 0
-  field :OPERATION_CREATE, 1
-  field :OPERATION_TOUCH, 2
-  field :OPERATION_DELETE, 3
+  field(:OPERATION_UNSPECIFIED, 0)
+  field(:OPERATION_CREATE, 1)
+  field(:OPERATION_TOUCH, 2)
+  field(:OPERATION_DELETE, 3)
 end
+
 defmodule Authzed.Api.V1.AlgebraicSubjectSet.Operation do
   @moduledoc false
   use Protobuf, enum: true, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :OPERATION_UNSPECIFIED, 0
-  field :OPERATION_UNION, 1
-  field :OPERATION_INTERSECTION, 2
-  field :OPERATION_EXCLUSION, 3
+  field(:OPERATION_UNSPECIFIED, 0)
+  field(:OPERATION_UNION, 1)
+  field(:OPERATION_INTERSECTION, 2)
+  field(:OPERATION_EXCLUSION, 3)
 end
+
 defmodule Authzed.Api.V1.Relationship do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :resource, 1, type: Authzed.Api.V1.ObjectReference, deprecated: false
-  field :relation, 2, type: :string, deprecated: false
-  field :subject, 3, type: Authzed.Api.V1.SubjectReference, deprecated: false
+  field(:resource, 1, type: Authzed.Api.V1.ObjectReference, deprecated: false)
+  field(:relation, 2, type: :string, deprecated: false)
+  field(:subject, 3, type: Authzed.Api.V1.SubjectReference, deprecated: false)
 
-  field :optional_caveat, 4,
+  field(:optional_caveat, 4,
     type: Authzed.Api.V1.ContextualizedCaveat,
     json_name: "optionalCaveat",
     deprecated: false
+  )
 end
+
 defmodule Authzed.Api.V1.ContextualizedCaveat do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :caveat_name, 1, type: :string, json_name: "caveatName", deprecated: false
-  field :context, 2, type: Google.Protobuf.Struct, deprecated: false
+  field(:caveat_name, 1, type: :string, json_name: "caveatName", deprecated: false)
+  field(:context, 2, type: Google.Protobuf.Struct, deprecated: false)
 end
+
 defmodule Authzed.Api.V1.SubjectReference do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :object, 1, type: Authzed.Api.V1.ObjectReference, deprecated: false
-  field :optional_relation, 2, type: :string, json_name: "optionalRelation", deprecated: false
+  field(:object, 1, type: Authzed.Api.V1.ObjectReference, deprecated: false)
+  field(:optional_relation, 2, type: :string, json_name: "optionalRelation", deprecated: false)
 end
+
 defmodule Authzed.Api.V1.ObjectReference do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :object_type, 1, type: :string, json_name: "objectType", deprecated: false
-  field :object_id, 2, type: :string, json_name: "objectId", deprecated: false
+  field(:object_type, 1, type: :string, json_name: "objectType", deprecated: false)
+  field(:object_id, 2, type: :string, json_name: "objectId", deprecated: false)
 end
+
 defmodule Authzed.Api.V1.ZedToken do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :token, 1, type: :string, deprecated: false
+  field(:token, 1, type: :string, deprecated: false)
 end
+
 defmodule Authzed.Api.V1.RelationshipUpdate do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :operation, 1,
+  field(:operation, 1,
     type: Authzed.Api.V1.RelationshipUpdate.Operation,
     enum: true,
     deprecated: false
+  )
 
-  field :relationship, 2, type: Authzed.Api.V1.Relationship, deprecated: false
+  field(:relationship, 2, type: Authzed.Api.V1.Relationship, deprecated: false)
 end
+
 defmodule Authzed.Api.V1.PermissionRelationshipTree do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  oneof :tree_type, 0
+  oneof(:tree_type, 0)
 
-  field :intermediate, 1, type: Authzed.Api.V1.AlgebraicSubjectSet, oneof: 0
-  field :leaf, 2, type: Authzed.Api.V1.DirectSubjectSet, oneof: 0
-  field :expanded_object, 3, type: Authzed.Api.V1.ObjectReference, json_name: "expandedObject"
-  field :expanded_relation, 4, type: :string, json_name: "expandedRelation"
+  field(:intermediate, 1, type: Authzed.Api.V1.AlgebraicSubjectSet, oneof: 0)
+  field(:leaf, 2, type: Authzed.Api.V1.DirectSubjectSet, oneof: 0)
+  field(:expanded_object, 3, type: Authzed.Api.V1.ObjectReference, json_name: "expandedObject")
+  field(:expanded_relation, 4, type: :string, json_name: "expandedRelation")
 end
+
 defmodule Authzed.Api.V1.AlgebraicSubjectSet do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :operation, 1,
+  field(:operation, 1,
     type: Authzed.Api.V1.AlgebraicSubjectSet.Operation,
     enum: true,
     deprecated: false
+  )
 
-  field :children, 2,
+  field(:children, 2,
     repeated: true,
     type: Authzed.Api.V1.PermissionRelationshipTree,
     deprecated: false
+  )
 end
+
 defmodule Authzed.Api.V1.DirectSubjectSet do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :subjects, 1, repeated: true, type: Authzed.Api.V1.SubjectReference
+  field(:subjects, 1, repeated: true, type: Authzed.Api.V1.SubjectReference)
 end
+
 defmodule Authzed.Api.V1.PartialCaveatInfo do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :missing_required_context, 1,
+  field(:missing_required_context, 1,
     repeated: true,
     type: :string,
     json_name: "missingRequiredContext",
     deprecated: false
+  )
 end
--- lib/api/v1/schema_service.pb.ex
+++ lib/api/v1/schema_service.pb.ex
@@ -2,29 +2,33 @@ defmodule Authzed.Api.V1.ReadSchemaRequest do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 end
+
 defmodule Authzed.Api.V1.ReadSchemaResponse do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :schema_text, 1, type: :string, json_name: "schemaText"
+  field(:schema_text, 1, type: :string, json_name: "schemaText")
 end
+
 defmodule Authzed.Api.V1.WriteSchemaRequest do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
-  field :schema, 1, type: :string, deprecated: false
+  field(:schema, 1, type: :string, deprecated: false)
 end
+
 defmodule Authzed.Api.V1.WriteSchemaResponse do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 end
+
 defmodule Authzed.Api.V1.SchemaService.Service do
   @moduledoc false
   use GRPC.Service, name: "authzed.api.v1.SchemaService", protoc_gen_elixir_version: "0.10.0"
 
-  rpc :ReadSchema, Authzed.Api.V1.ReadSchemaRequest, Authzed.Api.V1.ReadSchemaResponse
+  rpc(:ReadSchema, Authzed.Api.V1.ReadSchemaRequest, Authzed.Api.V1.ReadSchemaResponse)
 
-  rpc :WriteSchema, Authzed.Api.V1.WriteSchemaRequest, Authzed.Api.V1.WriteSchemaResponse
+  rpc(:WriteSchema, Authzed.Api.V1.WriteSchemaRequest, Authzed.Api.V1.WriteSchemaResponse)
 end
 
 defmodule Authzed.Api.V1.SchemaService.Stub do
